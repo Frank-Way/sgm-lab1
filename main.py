38b7327bd3f4d7b58940deebaca6e9b73b6c25fa
Анашкин Виталий Сергеевич <frerard.1992@mail.ru>
2022-10-05 23:33:28 +0300

    Добавлена полиномиальная аппроксимация функции двух переменных

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import math
 
 from matplotlib import pyplot as plt
 import csv
-from typing import List, Tuple
-
+from typing import List, Tuple, Callable
 
 Vector = List[float]   # список вещественных чисел
 Matrix = List[Vector]  # список векторов
@@ -19,14 +18,49 @@ def lin_space(start: float, stop: float, size: int) -> Vector:
     return result
 
 
-
-def get_rows_and_cols(matrix: Matrix) -> Tuple[int, int]:
+def get_rows_and_cols_count(matrix: Matrix) -> Tuple[int, int]:
     """
     Получение размеров матрицы
     :param matrix: матрица
     :return:       кортеж [строки, столбцы]
     """
     return len(matrix), len(matrix[0])
+
+
+def get_row(matrix: Matrix, row: int) -> Vector:
+    rows, cols = get_rows_and_cols_count(matrix)
+    result: Vector = list()
+    for col in range(cols):
+        result.append(matrix[row][col])
+    return result
+
+
+def get_col(matrix: Matrix, col: int) -> Vector:
+    rows, cols = get_rows_and_cols_count(matrix)
+    result: Vector = list()
+    for row in range(rows):
+        result.append(matrix[row][col])
+    return result
+
+
+def insert_row(matrix: Matrix, row: Vector, index: int) -> None:
+    matrix.insert(index, row)
+
+
+def add_row(matrix: Matrix, row: Vector) -> None:
+    rows, cols = get_rows_and_cols_count(matrix)
+    insert_row(matrix, row, rows)
+
+
+def insert_col(matrix: Matrix, col: Vector, index: int) -> None:
+    rows, cols = get_rows_and_cols_count(matrix)
+    for row in range(rows):
+        matrix[row].insert(index, col[row])
+
+
+def add_col(matrix: Matrix, col: Vector) -> None:
+    rows, cols = get_rows_and_cols_count(matrix)
+    insert_col(matrix, col, cols)
 
 
 def replace_col(matrix: Matrix, col: Vector, col_index: int) -> Matrix:
@@ -37,7 +71,7 @@ def replace_col(matrix: Matrix, col: Vector, col_index: int) -> Matrix:
     :param col_index: индекс столбца для замены в матрице
     :return:          матрица с замененным столбцом
     """
-    rows, cols = get_rows_and_cols(matrix)
+    rows, cols = get_rows_and_cols_count(matrix)
     result: Matrix = [[0 for j in range(cols)] for i in range(rows)]
     for j in range(cols):
         if j == col_index:
@@ -47,6 +81,14 @@ def replace_col(matrix: Matrix, col: Vector, col_index: int) -> Matrix:
             for i in range(rows):
                 result[i][j] = matrix[i][j]
     return result
+
+
+def copy_matrix(matrix: Matrix) -> Matrix:
+    return [copy_vector(matrix[i]) for i in range(len(matrix))]
+
+
+def copy_vector(vector: Vector) -> Vector:
+    return [vector[i] for i in range(len(vector))]
 
 
 def read_inputs(filename: str) -> Tuple[Vector, Vector]:
@@ -200,6 +242,9 @@ def calculate_determinatorN(matrix: Matrix, result: float = 0.0) -> float:
     """
     rows: int = len(matrix)
     cols: int = len(matrix[0])
+    if rows > 6:
+        import numpy
+        return numpy.linalg.det(matrix)
 
     if rows == 2:  # базовый случай - матрица 2х2
         return matrix[0][0] * matrix[1][1] - matrix[0][1] * matrix[1][0]
@@ -212,6 +257,22 @@ def calculate_determinatorN(matrix: Matrix, result: float = 0.0) -> float:
         sub_det: float = calculate_determinatorN(sub_matrix)  # определитель подматрицы
         result += sign * matrix[row_index][0] * sub_det       # разложение
 
+    return result
+
+
+def calculate_determinant(matrix: Matrix) -> float:
+    rows, cols = get_rows_and_cols_count(matrix)
+    extended = copy_matrix(matrix)
+    for row in range(rows - 1):
+        add_row(extended, get_row(matrix, row))
+    result = 0.0
+    for row in range(rows):
+        main_mul = 1.0
+        side_mul = 1.0
+        for col in range(cols):
+            main_mul *= extended[row + col][col]
+            side_mul *= extended[(rows - 1) + row - col][col]
+        result += main_mul - side_mul
     return result
 
 
@@ -671,11 +732,156 @@ def main_logarithmic_with_k(filename: str, is_plots_required: bool) -> Vector:
     return f
 
 
+PowerPair = Tuple[int, int]
+
+
+class PowerPairs:
+    @staticmethod
+    def get_range(start: int, stop: int, min_sum: int, max_sum: int) -> List[PowerPair]:
+        result = set()
+        for first in range(start, stop):
+            for second in range(start, stop):
+                result.add((first, second))
+        result = filter(lambda tup: min_sum <= sum(tup) <= max_sum, list(result))
+        result = sorted(result, key=lambda tup: (sum(tup), max(tup), tup[1]))
+        return list(result)
+
+    @staticmethod
+    def get_left_matrix(k: int) -> List[List[PowerPair]]:
+        powers = PowerPairs.get_range(0, k + 1, 0, k)
+        rows = len(powers)
+        result = [powers]
+        for i in range(1, rows):
+            row = []
+            for j in range(rows):
+                row.append(PowerPairs.mul(powers[i], powers[j]))
+            result.append(row)
+        return result
+
+    @staticmethod
+    def mul(pair1: PowerPair, pair2: PowerPair) -> PowerPair:
+        return pair1[0] + pair2[0], pair1[1] + pair2[1]
+
+    @staticmethod
+    def calculate_sum(x1: Vector, x2: Vector, cache_x: dict, powers: PowerPair, y: Vector = None, cache_y: dict = None):
+        n = len(x1)
+        if y is None:
+            if powers in cache_x.keys():
+                return cache_x[powers]
+            if sum(powers) == 0:
+                cache_x[powers] = n
+                return n
+            summa = 0.0
+            for i in range(n):
+                summa += x1[i] ** powers[0] * x2[i] ** powers[1]
+            cache_x[powers] = summa
+            return summa
+        else:
+            if powers in cache_y.keys():
+                return cache_y[powers]
+            summa = 0.0
+            for i in range(n):
+                summa += y[i] * x1[i] ** powers[0] * x2[i] ** powers[1]
+            cache_y[powers] = summa
+            return summa
+
+    @staticmethod
+    def calculate(k: int, x1: Vector, x2: Vector, y: Vector) -> Tuple[Matrix, Vector]:
+        powers_matrix = PowerPairs.get_left_matrix(k)
+        powers_vector = PowerPairs.get_range(0, k + 1, 0, k)
+        result1 = []
+        result2 = []
+        cache_x = dict()
+        cache_y = dict()
+        rows = len(powers_vector)
+        for i in range(rows):
+            row = []
+            for j in range(rows):
+                row.append(PowerPairs.calculate_sum(x1, x2, cache_x, powers_matrix[i][j]))
+            result1.append(row)
+            result2.append(PowerPairs.calculate_sum(x1, x2, cache_x, powers_vector[i], y, cache_y))
+        return result1, result2
+
+
+def main_polynomial_two_inputs(k: int, function: Callable, x_min: float, x_max: float, steps: int, is_plots_required: bool) -> Vector:
+    x1_linspace = lin_space(x_min, x_max, steps)
+    x2_linspace = lin_space(x_min, x_max, steps)
+    n = steps ** 2
+    y:  Vector = [0.0 for i in range(n)]
+    x1: Vector = [0.0 for i in range(n)]
+    x2: Vector = [0.0 for i in range(n)]
+    i = 0
+    for x1_value in x1_linspace:
+        for x2_value in x2_linspace:
+            x1[i] = x1_value
+            x2[i] = x2_value
+            y[i] = function((x1_value, x2_value))
+            i += 1
+    left, right = PowerPairs.calculate(k, x1, x2, y)
+
+    det: float = calculate_determinatorN(left)
+    if abs(det) < 1e-9:
+        raise ValueError
+    powers_vector = PowerPairs.get_range(0, k + 1, 0, k)
+    coefs_count = len(powers_vector)
+
+    coef_dets: Vector = [calculate_determinatorN(replace_col(left, right, i)) for i in range(coefs_count)]
+    coefs: Vector = [coef_det / det for coef_det in coef_dets]  # a0, a1, ..., ak
+
+    f: Vector = [sum([coefs[j] * (x1[i] ** powers_vector[j][0]) * (x2[i] ** powers_vector[j][1]) for j in range(coefs_count)]) for i in range(n)]  # аппроксимирующая функция
+
+
+    e: Vector = [f[i] - y[i] for i in range(n)]  # ошибки воспроизведения
+    e_square: Vector = [e[i] ** 2 for i in range(n)]  # квадраты ошибок
+
+    SE = calculate_squared_error(e)  # сумма квадратов ошибок
+
+    MAE = calculate_max_absolute_error(e)  # максимальная абсолютная ошибка
+
+    # вывод результатов
+
+    print("Определители:")
+    print(f"{det=}; " + "; ".join(map(lambda x: f"det_a{x}={coef_dets[x]}", range(len(coef_dets)))))
+
+    print("Коэффициенты полиномиальной аппроксимирующей функции:")
+    print("; ".join(map(lambda x: f"a{x}={coefs[x]}", range(len(coefs)))))
+
+    print("Мера отклонения:")
+    print(f"{SE=}")
+    print("Максимальная абсолютная ошибка:")
+    print(f"{MAE=}")
+
+    if is_plots_required:  # формирование графика
+        approximation = lambda args: sum(
+            [args[2][j] * (args[0] ** args[3][j][0]) * (args[1] ** args[3][j][1]) for j in range(len(args[3]))])
+
+        import numpy as np
+        fig = plt.figure()
+        ax1 = fig.add_subplot(1, 2, 1, projection='3d')
+        ax2 = fig.add_subplot(1, 2, 2, projection='3d')
+        x, y = np.linspace(x_min, x_max, 11), np.linspace(x_min, x_max, 11)
+        X, Y = np.meshgrid(x, y)
+        Z = approximation((X, Y, coefs, powers_vector))
+        F = np.sin(np.pi / 2 * X) * np.cos(np.pi / 2 * Y)
+
+        ax1.plot_surface(X, Y, Z, cmap='inferno')
+        ax1.set_xlabel("x")
+        ax1.set_ylabel("y")
+        ax1.set_zlabel("z")
+        ax1.set_title("Аппроксимация")
+        ax2.plot_surface(X, Y, F, cmap='inferno')
+        ax2.set_xlabel("x")
+        ax2.set_ylabel("y")
+        ax2.set_zlabel("z")
+        ax2.set_title("Исходная функция")
+        plt.show()
+
+    return f
 
 
 if __name__ == "__main__":
     filename = "input.csv"  # имя файла с исходными данными для аппроксимации
-    is_plots_required = True  # нужно ли строить графики
+    is_plots_required = False  # нужно ли строить графики
 
     # main_polynomial(filename, is_plots_required, 1)
     # main_polynomial(filename, is_plots_required, 2)
@@ -686,5 +892,12 @@ if __name__ == "__main__":
     # main_exponential(filename, is_plots_required)
     # main_exponential_with_k(filename, is_plots_required)
 
-    main_logarithmic(filename, is_plots_required)
-    main_logarithmic_with_k(filename, is_plots_required)
+    # main_logarithmic(filename, is_plots_required)
+    # main_logarithmic_with_k(filename, is_plots_required)
+
+
+    function = lambda x: math.sin(math.pi / 2 * x[0]) * math.cos(math.pi / 2 * x[1])
+    x_min = 0.0
+    x_max = 1.0
+    steps_count = 501
+    main_polynomial_two_inputs(7, function, x_min, x_max, steps_count, is_plots_required)
